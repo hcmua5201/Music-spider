41ce2793970df548fb24c5500c0345c6d98afdcb
hcmua5201 <1438679361@qq.com>
2024-09-14 16:05:40 +0800

    commit-开启多线程-executor.submit()改为executor.map()

--- music_2t58_com音乐爬虫.py
+++ music_2t58_com音乐爬虫.py
@@ -1,18 +1,32 @@
 import time
+from pprint import pprint
 
 import requests
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
+from selenium import webdriver
 import os
 
+# 自动化获取浏览器cookies
+driver = webdriver.Chrome()
+driver.get("http://music.2t58.com/")
+# 打印浏览器cookies
+cookies = driver.get_cookies()
+# 转为json格式
+cookies_json = {}
+for cookie in cookies:
+    cookies_json[cookie['name']] = cookie['value']
+pprint("浏览器cookies"+ str(cookies_json))
+driver.quit()
+
 cookies = {
-    'Hm_lvt_b8f2e33447143b75e7e4463e224d6b7f': '1726051102',
-    'HMACCOUNT': 'AD6D3F7F0821F20E',
+    'Hm_lvt_b8f2e33447143b75e7e4463e224d6b7f': cookies_json['Hm_lvt_b8f2e33447143b75e7e4463e224d6b7f'],
+    'HMACCOUNT': cookies_json['HMACCOUNT'],
     'mode': '1',
     'songIndex': '0',
     'coin_screen': '1707*1067',
     'b2d60fe00274e7c43e0c895f94b17153': '9ea2d361f75b2df0ed531eef54bf3dfc',
-    'Hm_lpvt_b8f2e33447143b75e7e4463e224d6b7f': '1726057805',
+    'Hm_lpvt_b8f2e33447143b75e7e4463e224d6b7f': cookies_json['Hm_lpvt_b8f2e33447143b75e7e4463e224d6b7f'],
     'down_mima': 'ok',
 }
 
@@ -21,7 +35,6 @@ headers = {
     'Accept-Language': 'zh-CN,zh;q=0.9',
     'Connection': 'keep-alive',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    # 'Cookie': 'Hm_lvt_b8f2e33447143b75e7e4463e224d6b7f=1726051102; HMACCOUNT=AD6D3F7F0821F20E; mode=1; songIndex=0; coin_screen=1707*1067; b2d60fe00274e7c43e0c895f94b17153=9ea2d361f75b2df0ed531eef54bf3dfc; Hm_lpvt_b8f2e33447143b75e7e4463e224d6b7f=1726057805; down_mima=ok',
     'Origin': 'http://music.2t58.com',
     'Referer': 'http://music.2t58.com/down.php?ac=music&id=ZHZ2c3Zzd3hz',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
@@ -59,15 +72,15 @@ def get_all_page(TotalData):
 
 
 # 3.下载歌曲并保存的函数
-def download_song(song_name, song_download_key):
+def download_song(params):
+    song_name, song_download_key = params
     start_time = time.time()
     response = requests.get(f"http://music.2t58.com/plug/down.php?ac=music&id={song_download_key}&k=320kmp3",
                             headers=headers, cookies=cookies)
     print(f"正在下载{song_name}----------------------------------------------------------------------------")
     # 判断是否已存在文件
     if os.path.exists(f'./{singer}/{song_name}.mp3'):
-        print(
-            f"{song_name}文件已存在,覆盖下载----------------------------------------------------------------------------")
+        print(f"{song_name}文件已存在,覆盖下载----------------------------------------------------------------------------")
 
     with open(f'./{singer}/{song_name}.mp3', 'wb') as f:
         f.write(response.content)
@@ -75,32 +88,39 @@ def download_song(song_name, song_download_key):
     download_time = end_time - start_time
     file_size = os.path.getsize(f'./{singer}/{song_name}.mp3')
     file_size_mb = file_size / (1024 * 1024)
-    print(
-        f"{song_name}下载完成，耗时{download_time:.2f}秒，文件大小为{file_size_mb:.2f}MB----------------------------------------------------------------------------")
+    print(f"{song_name}下载完成，耗时{download_time:.2f}秒，文件大小为{file_size_mb:.2f}MB----------------------------------------------------------------------------")
 
 
 # 4.获取每页的歌曲列表和下载关键id,并开启多线程下载
 def get_song_list(url, singer, AllPage):
-    count = 0
+    song_params = []
     for i in range(1, AllPage + 1):
         print(f"正在爬取第{i}页--------------------------------------------------------------------------------")
         res_url = f"{url}/{singer}/{i}.html"
         response = requests.get(res_url)
         soup = BeautifulSoup(response.text, "html.parser")
         song_list = soup.find_all("div", class_="play_list")
-        # 遍历song_list里面的所有ul下面的li
         for song in song_list:
             songs = song.find_all("li")
-            # count = 0  计数器放这就是每页重置
             for i in songs:
-                count = count + 1
                 really_songs = i.find_all("div", class_="name")
                 song_name = ' '.join(really_songs[0].text.split()).replace(' ', '')
                 song_download_key = really_songs[0].find('a')['href'].split('/')[-1][:-5]
-                print(f"第{count}首" + '  ' + song_name + '  ' + "下载地址关键值为" + song_download_key)
-                # 使用线程池下载歌曲
-                with ThreadPoolExecutor(max_workers=10) as executor:
-                    executor.submit(download_song, song_name, song_download_key)
+                song_params.append((song_name, song_download_key))
+
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        executor.map(download_song, song_params)
 
 
-
+if __name__ == "__main__":
+    TotalData = get_total_data(url, singer)
+    AllPage = get_all_page(TotalData)
+    # 开始时间
+    start_time = time.time()
+    get_song_list(url, singer, AllPage)
+    # 结束时间
+    end_time = time.time()
+    # 总耗时
+    total_time = end_time - start_time
+    print(f"总耗时{total_time:.2f}秒")
+    print("下载完成-")
